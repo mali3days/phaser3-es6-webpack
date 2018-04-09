b4636595bec625b73b9e0f440b079736f0f50887
Bugay_Nikolay <mali3days@gmail.com>
2018-04-09 21:49:40 +0300

    :sparkles: add new code: new constsnts, new variables, etc..

--- src/main.js
+++ src/main.js
@@ -1,50 +1,84 @@
 import 'phaser'
 
 const global = {
-  // base: 'src/assets',
-  // img: {
-  //   sky: 'sky',
-  //   ground: 'ground',
-  //   star: 'star',
-  //   bomb: 'bomb'
-  // },
+  base: 'src/assets',
+  elements: {
+    blue: 'blue',
+    green: 'green',
+    red: 'red',
+    yellow: 'yellow'
+  }
   // spr: {
   //   dude: 'dude'
   // }
 }
 
 const config = {
-  // type: Phaser.AUTO,
-  // parent: 'content',
-  // width: 800,
-  // height: 600,
-  // physics: {
-  //   default: 'arcade',
-  //   arcade: {
-  //     gravity: {
-  //       y: 300
-  //     },
-  //     debug: false
-  //   }
-  // },
+  type: Phaser.AUTO,
+  parent: 'content',
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      // gravity: {
+      //   y: 300
+      // },
+      debug: false
+    }
+  },
   scene: getState()
 }
 
+const game = new Phaser.Game(config)
+game.global = global
+
+const tileGrid = [
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null]
+]
+const tileTypes = [
+  'blue',
+  'green',
+  'red',
+  'yellow'
+]
+let score = 0
+let activeTile1 = null
+let activeTile2 = null
+let canMove = false
+
 function getState () {
   return {
-    // init () {
-    // },
+    init () {
+      //
+    },
+
     preload () {
+      const { base, elements } = global
+      // blue, green, red, yellow
+      for (let elem in elements) this.load.image(`${elem}`, `${base}/elements/${elem}.png`)
     },
 
     create () {
+      // this.backgroundColor = '#34495f'
+      let width = this.textures.get('blue').getSourceImage().width
+      let height = this.textures.get('blue').getSourceImage().height
+
+      this.physics.add.group({
+        key: 'blue'
+      })
+
+      const seed = Date.now()
+      this.random = Phaser.Math.RND
+      // console.log(this.random)
     },
 
     update () {
     }
   }
 }
-
-const game = new Phaser.Game(config)
-
-game.global = global
