990658cd3107b2e3e8c56cd91e60058d21813495
Bugay Nikolay <nikolay.bugay@nixsolutions.com>
2019-11-11 23:32:24 +0200

    :pencil: update hello world

--- src/main.js
+++ src/main.js
@@ -1,31 +1,17 @@
-import 'phaser'
-
-const global = {
-  base: 'src/assets',
-  elements: {
-    blue: 'blue',
-    green: 'green',
-    red: 'red',
-    yellow: 'yellow'
-  }
-}
-
-const config = {
+var config = {
   type: Phaser.AUTO,
-  parent: 'content',
   width: 800,
   height: 600,
   physics: {
     default: 'arcade',
     arcade: {
-      debug: false
+      gravity: { y: 200 }
     }
   },
   scene: getState()
 }
 
-const game = new Phaser.Game(config)
-game.global = global
+document.game = new Phaser.Game(config)
 
 function getState () {
   return {
@@ -33,15 +19,31 @@ function getState () {
     },
 
     preload () {
-      const { base, elements } = global
-      for (let elem in elements) this.load.image(`${elem}`, `${base}/elements/${elem}.png`)
+      this.load.setBaseURL('http://labs.phaser.io')
+
+      this.load.image('sky', 'assets/skies/space3.png')
+      this.load.image('logo', 'assets/sprites/phaser3-logo.png')
+      this.load.image('red', 'assets/particles/red.png')
     },
 
     create () {
-      this.physics.add.group({
-        key: 'blue'
+      this.add.image(400, 300, 'sky')
+
+      var particles = this.add.particles('red')
+
+      var emitter = particles.createEmitter({
+        speed: 100,
+        scale: { start: 1, end: 0 },
+        blendMode: 'ADD'
       })
-      this.random = Phaser.Math.RND
+
+      var logo = this.physics.add.image(400, 100, 'logo')
+
+      logo.setVelocity(100, 200)
+      logo.setBounce(1, 1)
+      logo.setCollideWorldBounds(true)
+
+      emitter.startFollow(logo)
     },
 
     update () {
